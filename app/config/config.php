301c677989b9fc8bc038794bf3101c8513d47535
RephaelATHLAN <rephaelathlan.devoir@gmail.com>
2025-12-07 20:23:28 +0100

    Mise en place env (.env), PDO Database et config projet

--- app/config/config.php
+++ app/config/config.php
@@ -2,12 +2,5 @@
 namespace App\Config;
 
 if (!defined('BASE_URL')) {
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
-    $host = $_SERVER['HTTP_HOST'];
-    
-    if ($host === 'localhost' || $host === '127.0.0.1') {
-        define('BASE_URL', $protocol . '://' . $host . '/projet_/public/');
-    } else {
-        define('BASE_URL', $protocol . '://' . $host . '/');
-    }
+    define('BASE_URL', rtrim($_ENV['APP_URL'], '/') . '/');
 }
