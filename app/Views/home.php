d05fd583ec9c39affb987397106e61ccb417c4ce
CostantiniYH <costantiniyaacov@gamil.com>
2025-12-07 21:47:17 +0100

    Présentation home

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,1 +1,9 @@
-<h1 class="contianer">Bienvenue sur  Gestion des matières ou Edu Prime</h1>
+<div class="contianer p-5 shadow">
+    <h1 class="text-center">Bienvenue sur  Gestion des matières ou Edu Prime</h1>
+    <p>Les gars je veux que tout le monde participe, je compte sur vous pour cartonner.</p>
+    <p>Si ce projet réussi, on enchaînera avec un site gestion des classes comme l'avait proposé Réphaël. Ensuite, 
+        nous pourrons faire un site e-commerce qui est un peu plus poussé.</p>
+    <p>Pour atteindre cette page en local avec : <code>php -S localhost:3000</code>, il faut vous positionner dans 
+    le répertoire de l'<code>index.php</code> qui est d'ailleurs la seulle entrée dans un design pattern MVC, avec la commande : <code>cd public</code> </p>
+</div>
+    
