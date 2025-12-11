3d2ec5859c97c1e4a2aaf31c797ac063cc765ee5
CostantiniYH <costantiniyaacov@gamil.com>
2025-12-11 17:47:03 +0100

    kljhl

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,9 +1,13 @@
-<div class="contianer p-5 shadow">
+<div class="contianer p-5 shadow-sm rounded-3 mb-3">
     <h1 class="text-center">Bienvenue sur  Gestion des matières ou Edu Prime</h1>
+    <h3>Equipe : Yaacov, Nathanel, Rephael, Ethan, Jonathan, Avi</h3>
+    <p>Ce projet a pour but de nous faire pratiquer le PHP en mode orienté objet avec le pattern MVC. 
+        Nous allons créer un site de gestion des matières pour un établissement scolaire. 
+        Chaque élève (dans un premier temps, ceux du SIO) pourra se connecter, voir les matières qu'il a, les professeurs, les notes etc...</p>
     <p>Les gars je veux que tout le monde participe, je compte sur vous pour cartonner.</p>
     <p>Si ce projet réussi, on enchaînera avec un site gestion des classes comme l'avait proposé Réphaël. Ensuite, 
         nous pourrons faire un site e-commerce qui est un peu plus poussé.</p>
     <p>Pour atteindre cette page en local avec : <code>php -S localhost:3000</code>, il faut vous positionner dans 
-    le répertoire de l'<code>index.php</code> qui est d'ailleurs la seulle entrée dans un design pattern MVC, avec la commande : <code>cd public</code> </p>
+    le répertoire de l'<code>index.php</code> qui est d'ailleurs la seule entrée dans un design pattern MVC, avec la commande : <code>cd public</code> </p>
 </div>
     
