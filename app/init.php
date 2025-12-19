43e1402a95a16656992c75389d19f77ad7d9fc7a
CostantiniYH <costantiniyaacov@gmail.com>
2025-12-19 10:37:59 +0100

    Problème de route résolu

--- app/init.php
+++ app/init.php
@@ -1,19 +1,20 @@
 <?php
 declare(strict_types=1);
 
+$configFile = __DIR__ . '/config/config.php';
+if (file_exists($configFile)) {
+    require $configFile;
+}
+
 use App\Config\Database;
 
 $pdo = Database::connect();
 echo "Connexion OK - ";
 
+require dirname(__DIR__) . '/routes/web.php'; 
 $router = new App\Core\Router($pdo);
 $router->run();
 
-require dirname(__DIR__) . '/routes/web.php';
 
 // require dirname(__DIR__) . '/test_db.php';
 
-$configFile = __DIR__ . '/config/config.php';
-if (file_exists($configFile)) {
-    require $configFile;
-}
